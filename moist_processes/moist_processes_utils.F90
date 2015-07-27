c72c3e2cb26ce0ff9f3f980bd919260d90f68ac4
William Cooke <William.Cooke@noaa.gov>
2015-07-27 16:37:38 -0400

    Backed out calculation of Maximum Thetae

--- moist_processes/moist_processes_utils.F90
+++ moist_processes/moist_processes_utils.F90
@@ -27,7 +27,7 @@ private
 
 !------------------ private and public data/interfaces -----------------
 
-public   capecalcnew, tempavg, column_diag, rh_calc, calc_thetae
+public   capecalcnew, tempavg, column_diag, rh_calc
 
 private  column_diag_1, column_diag_2, column_diag_3
 
@@ -558,72 +558,6 @@ subroutine rh_calc(pfull,T,qv,RH,do_simple,MASK, do_cmip)
         IF (present(MASK)) RH(:,:,:)=MASK(:,:,:)*RH(:,:,:)
 
 END SUBROUTINE rh_calc
-
-!#######################################################################
-
-SUBROUTINE calc_thetae(temp_c, mixing_ratio_c, pfull_c, thetae, maxTe_level)
-real,    dimension(:,:,:), intent(in)  :: temp_c,   &
-                                          mixing_ratio_c,   &
-                                          pfull_c
-real,    dimension(:,:,:), intent(out) :: thetae
-integer, dimension(:,:),   intent(out) :: maxTe_level
-
-!---------------------------------------------------------------------
-!   intent(in) variables:
-! 
-!     temp_c         Temperature field at model full levels [ deg K ]
-!     mixing_ratio_c Vapor mixing ratio at model full levels 
-!                     [ kg(h2o) / kg(dry air) ]
-!     pfull_c        Pressure field at large-scale model full levels 
-!                     [ Pa ]
-!
-!   intent(out) variables:
-!     
-!     thetae         The Equivalent Potential Temperature. [ deg K ]
-!     MaxTe_level    The level of maximum Equivalent Potential 
-!                    Temperature in the lowest 300hPa of the atmosphere. [ none]
-!
-!---------------------------------------------------------------------
-
-   integer :: i, j, k, kmax
-   real    :: max_thetae
-   real ::  Tlcl
-   real :: tmp, theta
-!--------------------------------------------------------------------
-!     Use equation 43 from Bolton (Mon. Wea. Rev. 1046-1053, 1980) to 
-!     define Thetae.
-!     Thetae = T*(1000/P)^(0.2854*(1-0.28e-3*r))           (r in g/kg)
-!               * exp((3.376/Tlcl - 0.00254)*r*(1+0.81e-3r)) 
-!--------------------------------------------------------------------
-
-   thetae(:,:,:) = 0.0
-   maxTe_level = 1
-   kmax = size(temp_c,3)
-   do j=1,size(temp_c,2)
-     do i=1,size(temp_c,1)
-       max_thetae = -999.9
-       do k= kmax,1,-1
-         tmp = 0.2854*(1.0-0.28*mixing_ratio_c(i,j,k)) !mixing_ratio here is kg/kg
-         theta = temp_c(i,j,k)*(100000.0/pfull_c(i,j,k))**tmp
-         ! Get vapor pressure (eqn 16 Bolton, 1980)
-         tmp = pfull_c(i,j,k)/100.0 * mixing_ratio_c(i,j,k)/(0.622 + mixing_ratio_c(i,j,k))
-         ! Get LCL temperature from eqn 21 Bolton 1980
-         Tlcl = 2840.0/(3.5*log(temp_c(i,j,k)) - log(tmp) -4.805) + 55.0
-         tmp = mixing_ratio_c(i,j,k)*1000.0*(1.0 + 0.81* mixing_ratio_c(i,j,k))
-         thetae(i,j,k) = theta* exp((3.376/Tlcl - 0.00254)*tmp)
-         if ( (pfull_c(i,j,kmax) - pfull_c(i,j,k)) < 30000.0 ) then
-           if ( thetae(i,j,k) > max_thetae) then
-             maxTe_level(i,j) = k
-             max_thetae       = thetae(i,j,k)
-           endif
-         endif
-       enddo
-     enddo
-   enddo
-        
-
-END SUBROUTINE calc_thetae
-
 
 !#######################################################################
 
