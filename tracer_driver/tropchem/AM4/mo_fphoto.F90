c4aab0ee2d96974c5f3bd766ad266a9cde109031
Jingqiu Mao <Jingqiu.Mao@gaea12.ncrc.gov>
2016-07-13 18:15:58 -0400

    Correct tags

--- tracer_driver/tropchem/AM4/mo_fphoto.F90
+++ tracer_driver/tropchem/AM4/mo_fphoto.F90
@@ -23,8 +23,8 @@
       private
       public :: fprate_init, fphoto
 
-      character(len=128)            :: version     = '$Id: mo_fphoto.F90,v 19.0 2012/01/06 20:33:54 fms Exp $'
-      character(len=128)            :: tagname     = '$Name: siena_201204 $'
+      character(len=128)            :: version     = '$Id$'
+      character(len=128)            :: tagname     = '$Name$'
 
       integer ::  fastjx_clock
       integer, parameter :: jdim     = JVN_     ! number of fastjx species 62
