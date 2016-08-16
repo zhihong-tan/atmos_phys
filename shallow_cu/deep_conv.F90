4fdb5611527394108116ed256f1810e3f032cf57
Ming Zhao <Ming.Zhao@gaea12.ncrc.gov>
2016-08-16 10:14:21 -0400

    add an additional namelist parameter rkm_max

--- shallow_cu/deep_conv.F90
+++ shallow_cu/deep_conv.F90
@@ -147,6 +147,7 @@ contains
     dpn % use_new_let        = cpn % use_new_let
     dpn % do_tten_max        = cpn % do_tten_max
     dpn % tten_max           = cpn % tten_max
+    dpn % rkm_max            = cpn % rkm_max
     dpn % scaleh0            = cpn % scaleh0
     dpn % beta               = cpn % beta
 
