60b1ce3dce53bfdd04c167972f1fe2518852456a
Larry Horowitz <Larry.Horowitz@noaa.gov>
2018-04-03 14:12:01 -0400

    Bugfix for undefined values in dry deposition

--- tracer_driver/atmos_tracer_utilities.F90
+++ tracer_driver/atmos_tracer_utilities.F90
@@ -800,7 +800,13 @@ subroutine dry_deposition( n, is, js, u, v, T, pwt, pfull, dz, &
     !f1p
     !alpha = max(min(1.7e-6*hwindv**3.75,1.),0.) !WU 1979
     !alpha = max(min(1.e-6*u_star**3,0.) !Wu 1988, variations of whitecap coverage with wind stress and water temperature
-    alpha = max(min(2.81e-5*(hwindv-3.87)**2.76,1.),0.) !Observations of whitecap coverage and the relation to wind stress, wave slope, and turbulent dissipation, Schwendeman and Thomson, 2016, JGR ocean
+!   alpha = max(min(2.81e-5*(hwindv-3.87)**2.76,1.),0.) !Observations of whitecap coverage and the relation to wind stress, wave slope, and turbulent dissipation, Schwendeman and Thomson, 2016, JGR ocean
+!Observations of whitecap coverage and the relation to wind stress, wave slope, and turbulent dissipation, Schwendeman and Thomson, 2016, JGR ocean
+    where (hwindv .gt. 3.87)
+       alpha = max(min(2.81e-5*(hwindv-3.87)**2.76,1.),0.)
+    elsewhere
+       alpha = 0.
+    endwhere
 
     kss   = frictv/sear
     kbs   = 50. !set to very high value
